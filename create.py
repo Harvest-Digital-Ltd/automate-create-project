0064a4cc1fe59d247d593e44ebd619f5a9d098d7
Arianna Fischer <arianna.fischer@harvestdigital.com>
2022-05-17 17:55:43 +0100

    can create remote repo with command line args

--- create.py
+++ create.py
@@ -1,22 +1,52 @@
 from github import Github
-import sys
+import argparse
+import sys, getopt
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-access_token = os.getenv("ACCESS_TOKEN")
+_ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
+_HOME = os.path.expanduser("~")
 
-def create():
-    folder_name = str(sys.argv[1])
-    g = Github(access_token)
+def command_parser():
+    parser = argparse.ArgumentParser(
+        description="Creates remote GitHub repository and clones it locally."
+    )
+    # The following argument(s) should be provided to run the example.
+    parser.add_argument(
+        "github_repo",
+        type=str,
+        help="The GitHub repository to be created remotely and cloned locally."
+    )
+    parser.add_argument(
+        "-w",
+        "--workspace_path",
+        type=str,
+        help=f"The path to the default workspace. By default, this is {os.path.join(_HOME, 'Documents')}"
+    )
+    args = parser.parse_args()
+    return args
+    
+
+def create(args):
+    repo_name = args.github_repo
+    workspace = args.workspace_path
+    if not workspace:
+        workspace = os.path.join(_HOME, "Documents")
+    g = Github(_ACCESS_TOKEN)
     u = g.get_user()
-    r = u.create_repo(folder_name) 
-    r.create_file('README.md', 'initial commit', '# {}'.format(folder_name)) 
-    print("Succesfully created remote repository {}".format(folder_name))
-    os.chdir('/home/quibbleahr/Documents/workspace')
-    os.system('git clone https://github.com/quibbleahr/{}'.format(folder_name))
-    print("Succesfully created local repository {}".format(folder_name))
+    r = u.create_repo(repo_name)
+    r.create_file('README.md', 'initial commit', f'# {repo_name}') 
+    print(f"Succesfully created remote repository {repo_name}")
+    os.chdir(workspace)
+    os.system(f"git clone git@github.com:ariannafharvest/{repo_name}")
+    print(f"Succesfully created local repository {repo_name}")
+    os.chdir(os.path.join(os.getcwd(),f"{repo_name}"))
+
+def main():
+    args = command_parser()
+    create(args)
 
 if __name__ == '__main__':
-    create()
+    main()
